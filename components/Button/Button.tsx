6e185a585bfb19c977d4a7cfc3039ad65b17eed8
Yawar M <muhammad.yawar@10pearls.com>
2023-10-25 17:13:53 +0500

    - Working native/tailWind setup

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -7,20 +7,6 @@ export interface Props {
   label: string;
   onClick?: () => void;
 }
-
-// export const MyButton = ({text, onPress, color, textColor}: ButtonProps) => (
-//   <View style={styles.buttonContainer}>
-//     <TouchableOpacity
-//       style={[styles.button, !!color && {backgroundColor: color}]}
-//       onPress={onPress}
-//       activeOpacity={0.8}>
-//       <Text style={[styles.buttonText, !!textColor && {color: textColor}]}>
-//         {text}
-//       </Text>
-//     </TouchableOpacity>
-//   </View>
-// );
-
 const themeTest = {
   village: {
     primary: 'green',
@@ -31,8 +17,6 @@ const themeTest = {
     secondary: 'blue',
   },
 };
-// const StyledText = styled(Text);
-// const StyledTouchableOpacity = styled(TouchableOpacity);
 const Button = ({variant = 'primary', label, onClick}: Props) => {
   const {theme} = useThemeProvider();
   const color = themeTest[theme][variant];
@@ -41,8 +25,8 @@ const Button = ({variant = 'primary', label, onClick}: Props) => {
     <TouchableOpacity
       className={`bg-${color}-500 text-white px-4 py-2 rounded-lg`}
       onPress={onClick}>
-      <View>
-        <Text className="text-red">{label}</Text>
+      <View className="bg-black">
+        <Text className="text-red-500">{label}</Text>
       </View>
     </TouchableOpacity>
   );
